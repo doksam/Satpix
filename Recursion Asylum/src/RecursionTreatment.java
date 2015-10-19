534dc3e9dd9dfef26c08e8c39b9cc6efec2e7ace
Cameron <Cameron@x220no2.internal.hws.org>
2015-10-19 09:08:40 -0700

    Signed-off-by: Cameron <Cameron@x220no2.internal.hws.org>

---
+++ Recursion Asylum/src/RecursionTreatment.java
@@ -0,0 +1,12 @@
+
+public class RecursionTreatment {
+
+	public static void main(String[] args) {
+		
+		System.out.println(RecursionPsychologicalProblems.factorial(5));
+        int[] newArray = {1,2,3,4,5,6,7};
+        RecursionPsychologicalProblems.printArray(newArray, 7);
+        RecursionPsychologicalProblems.printBackwards(newArray, 7);
+	}
+
+}
